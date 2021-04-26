8d8b5b61b21b1d60a9b0d7e99d73b0a79cfe61a4
SmolUpsilon <83137147+SmolUpsilon@users.noreply.github.com>
2021-04-26 21:48:42 +0100

    Add files via upload

---
+++ plswork.py
@@ -0,0 +1,6 @@
+a = "This is Atlantis"
+a = input ("Earth Central Recieving")
+if (a == "This is Atlantis"):
+    print("It's good to hear from you Atlantis, we thought we'd lost you")
+else:
+    print("Communication terminated")
